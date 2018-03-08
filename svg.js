230e412ae03e4c3427eba2944b5779d4106c22ca
Jacqueline Xu <jackiestuy@gmail.com>
2018-03-08 14:43:17 -0500

    probably done

--- svg.js
+++ svg.js
@@ -3,37 +3,41 @@ var coords = document.getElementById('coords');
 var clear = document.getElementById('clear');
 var mouseX;
 var mouseY;
-var radius = 10;
+var radius = 20;
+var startColor = "red";
 var ns = "http://www.w3.org/2000/svg";
 
-var newCircle = function(x, y, r){
+var newCircle = function(x, y, r, color){
     var c = {
-       circle: document.createElementNS(ns, "circle"),
-       changeColor: function(e){
-            if(this.getAttribute("fill") == "red"){
-                this.setAttribute("fill", "green");
-            }
-            else{
-                this.setAttribute("fill", "red");
-            }
-            e.stopPropagation();
-       },
-       add: function(){
-        svg.appendChild(this.circle);
-       },
-       deleteSpawn: function(e){
-        this.remove();
-        var circle = newCircle(Math.random() * svg.getBoundingClientRect().width, Math.random() * svg.getBoundingClientRect().height, radius);
-        circle.add();
-        coords.value = "["+mouseX+","+mouseY+"]";
-        e.stopPropagation();
+      xcor: x,
+      ycor: y,
+      radius: r,
+      circle: document.createElementNS(ns, "circle"),
+      color: color,
+      changeColor: function(e){
+        if(this.getAttribute("fill") == "red"){
+          this.setAttribute("fill", "green");
+          this.color = "green";
         }
+        else {
+          e.target.remove(e);
+          var circle = newCircle(Math.random() * svg.getBoundingClientRect().width, Math.random() * svg.getBoundingClientRect().height, radius, startColor);
+          circle.display();
+          coords.value = "["+mouseX+","+mouseY+"]";
+        }
+        e.stopPropagation();
+      },
+      display: function(){
+        svg.appendChild(this.circle);
+      },
+      remove: function(e){
+        delete e.target;
+      }
     }
     c.circle.setAttribute("cx", x);
     c.circle.setAttribute("cy", y);
     c.circle.setAttribute("r", r);
-    c.circle.setAttribute("fill", "red");
-    c.circle.addEventListener("dblclick", c.deleteSpawn);
+    c.circle.setAttribute("fill", color);
     c.circle.addEventListener("click", c.changeColor, true);
 
     return c;
@@ -43,8 +47,8 @@ var placeDot = function(e){
     var bounds = svg.getBoundingClientRect();
     mouseX = e.pageX - bounds.left - scrollX;
     mouseY = e.pageY - bounds.top - scrollY;
-    var circle = newCircle(mouseX, mouseY, radius);
-    circle.add();
+    var circle = newCircle(mouseX, mouseY, radius, startColor);
+    circle.display();
     coords.value = "["+mouseX+","+mouseY+"]";
     e.stopPropagation();
 }
